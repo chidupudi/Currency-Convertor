823f2749c8c17fff257f057548294e12a69b36a9
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-08-17 12:51:44 +0530

    changes

--- app.py
+++ app.py
@@ -3,15 +3,24 @@ import requests
 
 app = Flask(__name__)
 
-API_KEY = 'your_api_key'  # Replace with your currency API key
+API_KEY = '9aac7313f2e53bd5f716256c'
 API_URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/"
 
 def get_supported_currencies():
-    response = requests.get(API_URL + "USD")
-    data = response.json()
-    if data['result'] == 'success':
-        return data['conversion_rates'].keys()
-    return []
+    try:
+        response = requests.get(API_URL + "USD")
+        response.raise_for_status()  # Check if the request was successful
+        data = response.json()
+        if data['result'] == 'success':
+            return data['conversion_rates'].keys()
+        else:
+            return []
+    except requests.exceptions.JSONDecodeError:
+        print("Error: Failed to decode JSON response")
+        return []
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -22,12 +31,13 @@ def index():
         amount = float(request.form.get('amount'))
 
         response = requests.get(API_URL + from_currency)
+        response.raise_for_status()  # Check if the request was successful
         data = response.json()
 
         if data['result'] == 'success':
             conversion_rate = data['conversion_rates'][to_currency]
             converted_amount = round(amount * conversion_rate, 2)
-            return render_template('index.html', converted_amount=converted_amount, from_currency=from_currency, to_currency=to_currency, currencies=currencies)
+            return render_template('index.html', converted_amount=converted_amount, from_currency=from_currency, to_currency=to_currency, amount=amount, currencies=currencies)
         else:
             return render_template('index.html', error="Error in fetching data. Please try again.", currencies=currencies)
     return render_template('index.html', currencies=currencies)
