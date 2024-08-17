5b4c970deb85abdf13b056a972f99045d13be033
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-08-17 12:20:36 +0530

    changes

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 API_KEY = 'your_api_key'  # Replace with your currency API key
-API_URL = f"https://v6.exchangerate-api.com/v6/9aac7313f2e53bd5f716256c/latest /"
+API_URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/"
+
+def get_supported_currencies():
+    response = requests.get(API_URL + "USD")
+    data = response.json()
+    if data['result'] == 'success':
+        return data['conversion_rates'].keys()
+    return []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    currencies = get_supported_currencies()
     if request.method == 'POST':
         from_currency = request.form.get('from_currency')
         to_currency = request.form.get('to_currency')
@@ -19,10 +27,10 @@ def index():
         if data['result'] == 'success':
             conversion_rate = data['conversion_rates'][to_currency]
             converted_amount = round(amount * conversion_rate, 2)
-            return render_template('index.html', converted_amount=converted_amount, from_currency=from_currency, to_currency=to_currency)
+            return render_template('index.html', converted_amount=converted_amount, from_currency=from_currency, to_currency=to_currency, currencies=currencies)
         else:
-            return render_template('index.html', error="Error in fetching data. Please try again.")
-    return render_template('index.html')
+            return render_template('index.html', error="Error in fetching data. Please try again.", currencies=currencies)
+    return render_template('index.html', currencies=currencies)
 
 if __name__ == '__main__':
     app.run(debug=True)
